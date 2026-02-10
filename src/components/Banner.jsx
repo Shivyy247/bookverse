f60964ae44c837db5efbeb9f3c257e318c0b2115
Shivani Barman <shivanibarman247@gmail.com>
2026-02-10 22:13:33 +0530

    Add Banner component to Home page

---
+++ src/components/Banner.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react'
+import { container, formContainer, headerText, inputField, inputWrapper, paragraphText, searchButton, subHeader } from "../assets/BOOKSELLER/dummystyles";
+import { geometricOverlay, glassBox } from '../assets/BOOKSELLER/dummystyles';
+import { useNavigate } from 'react-router-dom';
+import { words } from '../assets/BOOKSELLER/dummydata';
+import { Search } from 'lucide-react';
+
+const Banner = () => {
+
+    const [searchQuery, setSearchQuery] = useState("")
+    const [currentWord, setCurrentWord] = useState(0)
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentWord( (prev) => (prev+1) % words.length)
+        }, 2000)
+        return () => clearInterval(interval)
+    }, [])
+    
+    const handleSearch = (e) => {
+        e.preventDefault()
+        if (searchQuery.trim()) {
+            navigate(`/books?search=${encodeURIComponent(searchQuery.trim())}`)
+        }
+    }
+
+  return (
+    <div className={container}>
+          <div className={glassBox}>
+              <div className={geometricOverlay}>
+                  <div className='absolute -top-10 -right-10 md:-top-20 md:-right-20 w-48 h-48 md:w-96 md:h-96 bg-[#F8FFAE]/10 rounded-full blur-xl md:blur-3xl'/>
+                  <div className='absolute -bottom-20 -left-20 md:bottom-40 md:-left-40 h-40 w-40 md:w-80 md:h-80 bg-[#43C6AC]/10 rounded-full blur-xl md:blur-3xl'/>
+              </div>
+              
+              <div className='grid lg:grid-cols-2 gap-8 md:gap-12 items-center'>
+                  {/* TEXT CENTER */}
+                  <div className='space-y-6 md::space-y-8'>
+                      <div className='space-y-4 md:space-y-6'>
+                          <h1 className={headerText}>
+                              <span className='text-transparent bg-clip-text bg-linear-to-r from-[#2B5876] to-[#43C6AC]'>
+                                  MindFull
+                              </span>
+                              <br />
+                              <span className={subHeader}>
+                                  Reading Experience
+                              </span>
+                          </h1>
+                          <p className={paragraphText}>
+                              Curated knowledge journeys that challenge perceptons and inspire growth.
+                              Discover transformative content crafted for the modern intellect.
+                          </p>
+                      </div>
+
+                      {/* SEARCH FUNCTION */}
+
+                      <form onSubmit={handleSearch} className='space-y-6 md:space-y-8'>
+                          <div className={formContainer}>
+                              <div className={inputWrapper}>
+                                  <div className='absolute inset-0 bg-white/90 rounded-lg md:rounded-xl shadow-sm' />
+                                  <div className='relative flex items-center'>
+                                      <Search className='ml-4 md:ml-5 w-5 h-5 md:w-6 md:h-6 text-gray-600 group-focus-within:text-[#2B5876]' />
+                                      <input type='text' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} placeholder='Search Author, titles, or concept...' className={inputField}/>
+                                      
+                                  </div>
+                              </div>
+                              <button type='submit' className={searchButton}>
+                                  <Search className='w-4 h-4 md:w-5 md:h-5' />
+                                  <span className='sr-only'>Search</span>
+                                  
+                              </button>
+                          </div>
+                      </form>
+
+                      
+                  </div>
+              </div>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
